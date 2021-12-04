9c29f612bd5619ba95ee04e0dfb8a24bddbf224e
kunip004 <prajesh.x.kunimal.-nd@disney.com>
2021-12-04 14:01:56 -0500

    Feature/HLA_MVP_100 : added the project pom and modules.

---
+++ customer-process/src/main/java/com/hla/loan/CustomerProcessApplication.java
@@ -0,0 +1,13 @@
+package com.hla.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProcessApplication.class, args);
+	}
+
+}
---
+++ customer-validation/src/main/java/com/hla/loan/CustomerValidationApplication.java
@@ -0,0 +1,13 @@
+package com.hla.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerValidationApplication.class, args);
+	}
+
+}
